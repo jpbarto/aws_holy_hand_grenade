41e0525e929898267d5bd274ea6193063112e9b7
Jason Barto <jason.p.barto@gmail.com>
2019-02-08 22:19:33 +0000

    fixed errors in the shell script

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM alpine:latest
 COPY aws-nuke-v2.7.0-linux-amd64 /aws-nuke
+COPY run_aws_nuke.sh /run_aws_nuke.sh
+COPY aws-nuke.conf /aws-nuke.conf
 CMD ["/run_aws_nuke.sh"]
--- run_aws_nuke.sh
+++ run_aws_nuke.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+echo "Configuring environment to nuke AWS account"
+env
+
 # update the configuration file for the current environment
 # update the targeted account number
-sed -i .bak -e 's/TARGET-AWS-ACCOUNT-NUMBER/'"$AWS_ACCOUNT_ID"'/g' aws-nuke.conf
+sed -i.bak -e 's/TARGET-AWS-ACCOUNT-NUMBER/'"$AWS_ACCOUNT_ID"'/g' aws-nuke.conf
 
-/aws-nuke --force --no-dry-run -c /aws-nuke.conf
+echo "aws-nuke configuration file follows..."
+cat /aws-nuke.conf
+echo "END OF FILE"
+
+echo "Running aws-nuke..."
+/aws-nuke --force --no-dry-run \
+    -c /aws-nuke.conf \
+    --access-key-id ${AWS_ACCESS_KEY_ID} \
+    --secret-access-key ${AWS_SECRET_ACCESS_KEY} \
+    --session-token ${AWS_SESSION_TOKEN}
