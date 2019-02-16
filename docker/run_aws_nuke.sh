34d4f4754103064bd7640ff539cb686e78b54558
Jason Barto <jason.p.barto@gmail.com>
2019-02-16 19:57:23 +0000

    added the newly located docker files

---
+++ docker/run_aws_nuke.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Configuring environment to nuke AWS account"
+env
+
+# update the configuration file for the current environment
+# update the targeted account number
+sed -i.bak -e 's/TARGET-AWS-ACCOUNT-NUMBER/'"$TARGET_AWS_ACCOUNT_NUMBER"'/g' aws-nuke.conf
+sed -i.bak -e 's,HHG-ECS-CLUSTER,'"$HHG_ECS_CLUSTER"',g' aws-nuke.conf
+sed -i.bak -e 's,HHG-LOG-GROUP,'"$HHG_LOG_GROUP"',g' aws-nuke.conf
+sed -i.bak -e 's/HHG-IAM-ROLE/'"$HHG_IAM_ROLE"'/g' aws-nuke.conf
+
+echo "aws-nuke configuration file follows..."
+cat /aws-nuke.conf
+echo "END OF FILE"
+
+echo "Running aws-nuke..."
+timeout -t 3600 /aws-nuke --force --no-dry-run \
+    -c /aws-nuke.conf \
+    --profile default
+
+echo "aws-nuke run complete"
